697deb71d5f871ea4127b476a18a83c5ee152ede
sentencebird <hoge@example.com>
2021-06-20 17:39:48 +0900

    update: pyannotate.audioで音声区間検出してノイズ区間特定

--- app.py
+++ app.py
@@ -6,6 +6,33 @@ import soundfile as sf
 import numpy as np
 import plotly.graph_objects as go
 
+from pyannote.audio.utils.signal import Binarize
+import torch
+
+@st.cache
+def speech_activity_detection_model():
+    sad = torch.hub.load('pyannote/pyannote-audio', 'sad_ami')
+    sad = torch.hub.load('pyannote/pyannote-audio', 'sad_ami', device='cpu', batch_size=128)
+    return sad
+
+def trim_noise_part_from_speech(sad, fname):
+    file_obj = {"uri": "filename", "audio": fname}
+    sad_scores = sad(file_obj)
+    binarize = Binarize(offset=0.52, onset=0.52, log_scale=True, min_duration_off=0.1, min_duration_on=0.1)
+    speech = binarize.apply(sad_scores, dimension=1)
+    
+    speech_wav, sr = librosa.load(fname, sr=None, mono=False)
+    noise_wav = np.zeros((speech_wav.shape[0], 0))
+    append_axis = 1 if speech_wav.ndim == 2 else 0
+    noise_ranges = []
+    noise_start = 0
+    for segmentation in speech.segmentation():
+        noise_end, next_noise_start = int(segmentation.start*sr), int(segmentation.end*sr)
+        noise_wav = np.append(noise_wav, speech_wav[:, noise_start:noise_end], axis=append_axis)
+        noise_ranges.append((noise_start/sr, noise_end/sr))
+        noise_start = next_noise_start
+    return noise_wav.T, noise_ranges
+
 def trim_audio(data, rate, start_sec=None, end_sec=None):
     start, end = int(start_sec * rate), int(end_sec * rate)
     if data.ndim == 1: # mono
@@ -30,19 +57,28 @@ st.subheader('Original audio')
 st.audio(uploaded_file)
 
 st.subheader('Noise part')
-default_ranges = (0.0, float(wav_seconds)) if is_file_uploaded else (73.0, float(wav_seconds))
-noise_part_ranges = st.slider("Select a part of the noise (sec)", 0.0, float(wav_seconds), default_ranges, step=0.1)
-
+noise_part_detection_method = st.radio('Noise source detection', ['Manually', 'Automatically (using speech activity detections)'])
+if noise_part_detection_method == "Manually": # ノイズ区間は1箇所
+    default_ranges = (0.0, float(wav_seconds)) if is_file_uploaded else (73.0, float(wav_seconds))
+    noise_part_ranges = [st.slider("Select a part of the noise (sec)", 0.0, float(wav_seconds), default_ranges, step=0.1)]
+    noise_wav = trim_audio(wav, sr, noise_part_ranges[0][0], noise_part_ranges[0][1])
+    
+elif noise_part_detection_method == "Automatically (using speech activity detections)": # ノイズ区間が複数
+    with st.spinner('Please wait for Detecting the speech activities'):
+        sad = speech_activity_detection_model()
+        noise_wav, noise_part_ranges = trim_noise_part_from_speech(sad, uploaded_file)
+   
 fig = go.Figure()
 x_wav = np.arange(len(wav)) / sr
 fig.add_trace(go.Scatter(y=wav[::1000]))
-fig.add_vrect(x0=int(noise_part_ranges[0]*sr/1000), x1=int(noise_part_ranges[1]*sr/1000), fillcolor="Red", opacity=0.2)
+for noise_part_range in noise_part_ranges:
+    fig.add_vrect(x0=int(noise_part_range[0]*sr/1000), x1=int(noise_part_range[1]*sr/1000), fillcolor="Red", opacity=0.2)
 fig.update_layout(width=700, margin=dict(l=0, r=0, t=0, b=0, pad=0))
 fig.update_yaxes(visible=False, ticklabelposition='inside', tickwidth=0)
 st.plotly_chart(fig, use_container_with=True)
 
 st.text('Noise audio')
-sf.write('noise_clip.wav', trim_audio(wav, sr, noise_part_ranges[0], noise_part_ranges[1]), sr)
+sf.write('noise_clip.wav', noise_wav, sr)
 noise_wav, sr = librosa.load('noise_clip.wav', sr=None)
 st.audio('noise_clip.wav')
 
